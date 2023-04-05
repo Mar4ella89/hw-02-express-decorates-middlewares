179de420513847578ab151bd44bb99a3b7312302
Marat Baikin <3371335@gmail.com>
2023-04-05 18:00:30 +0300

    add decorates and middlewares

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,93 +1,48 @@
-const Joi = require("joi");
-
 const contacts = require("../models/contacts");
-const { HttpError } = require("../helpers");
+const { HttpError, ctrlWrapper } = require("../helpers");
 
-const addSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required().messages({
-    "any.required": `"name" is required`,
-    "string.empty": `"name" cannot be empty`,
-    "string.base": `"name" must be string`,
-  }),
-  email: Joi.string()
-    .email({
-      minDomainSegments: 2,
-    })
-    .required()
-    .messages({
-      "any.required": `"email" is required`,
-      "string.empty": `"email" cannot be empty`,
-    }),
-  phone: Joi.number().integer().required().messages({
-    "any.required": `"phone" is required`,
-    "string.empty": `"phone" cannot be empty`,
-  }),
-});
-
-const getAll = async (req, res, next) => {
-  try {
-    const allContacts = await contacts.listContacts();
-    res.json(allContacts);
-  } catch (error) {
-    next(error);
-  }
+const getAll = async (req, res) => {
+  const allContacts = await contacts.listContacts();
+  res.json(allContacts);
 };
 
-const getById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const contact = await contacts.getContactById(id);
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const contact = await contacts.getContactById(id);
 
-    if (!contact) {
-      throw HttpError(404, "Contact not found");
-    }
-    res.json(contact);
-  } catch (error) {
-    next(error);
+  if (!contact) {
+    throw HttpError(404, "Contact not found");
   }
+  res.json(contact);
 };
 
-const add = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error);
-    }
-    const newContact = await contacts.addContact(req.body);
-    res.status(201).json(newContact);
-  } catch (error) {
-    next(error);
-  }
+const add = async (req, res) => {
+  const newContact = await contacts.addContact(req.body);
+  res.status(201).json(newContact);
 };
 
-const updareById = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, "Missing fields");
-    }
-    const { id } = req.params;
-    const updateContact = await contacts.updateContact(id, req.body);
-    if (!updateContact) {
-      throw HttpError(404, "Contact not found");
-    }
-    res.json(updateContact);
-  } catch (error) {
-    next(error);
+const updareById = async (req, res) => {
+  const { id } = req.params;
+  const updateContact = await contacts.updateContact(id, req.body);
+  if (!updateContact) {
+    throw HttpError(404, "Contact not found");
   }
+  res.json(updateContact);
 };
 
-const deleteById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const deleteContact = await contacts.removeContact(id);
-    if (!deleteContact) {
-      throw HttpError(404, "Contact not found");
-    }
-    res.json({ message: "Contact deleted" });
-  } catch (error) {
-    next(error);
+const deleteById = async (req, res) => {
+  const { id } = req.params;
+  const deleteContact = await contacts.removeContact(id);
+  if (!deleteContact) {
+    throw HttpError(404, "Contact not found");
   }
+  res.json({ message: "Contact deleted" });
 };
 
-module.exports = { getAll, getById, add, updareById, deleteById };
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updareById: ctrlWrapper(updareById),
+  deleteById: ctrlWrapper(deleteById),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,19 @@
 const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
+
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../schemas/contacts");
+
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
 router.get("/:id", ctrl.getById);
 
-router.post("/", ctrl.add);
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:id", ctrl.updareById);
+router.put("/:id", validateBody(schemas.addSchema), ctrl.updareById);
 
 router.delete("/:id", ctrl.deleteById);
 
